6e97112c99182f01aec2424d5c354a5699627af9
Tilen Komel <tilen.komel10@gmail.com>
2022-12-11 22:41:33 +0100

    Added S3 config testing

--- app/init.js
+++ app/init.js
@@ -1,4 +1,7 @@
-console.log(`
+const { s3client, ListObjectsCommand } = require("./s3");
+
+const init = async () => {
+  console.log(`
  _____             _               ____             _                
 |  __ \\           | |             |  _ \\           | |               
 | |  | | ___   ___| | _____ _ __  | |_) | __ _  ___| | ___   _ _ __  
@@ -8,3 +11,22 @@ console.log(`
                                                               | |    
                                                               |_|    
 `);
+  console.log("[TEST] Environment variables");
+  if (process.env.DO_BA_S3) {
+    console.log("  [S3]   'DO_BA_S3 = true', testing S3 config");
+
+    let code = await s3client.send(
+      new ListObjectsCommand({
+        Bucket: process.env.DO_BA_S3_BUCKET,
+      })
+    );
+
+    if (code.$metadata.httpStatusCode != 200) {
+      console.log("[S3 ERROR] Http status code: " + code);
+      process.exit(1);
+    }
+    console.log("  [S3]   Config OK");
+  }
+};
+
+init();
